31d81c0a119860992158a2171fb9e2b3c17ff535
NataliaVK <bam.net@ngs.ru>
2015-01-24 00:26:01 +0600

    update sqls

--- src/main/java/com/shop/backend/domain/Orders.java
+++ src/main/java/com/shop/backend/domain/Orders.java
@@ -1,6 +1,9 @@
 package com.shop.backend.domain;
 
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
 import com.shop.backend.utils.DateUtils;
+import org.hibernate.annotations.Formula;
 import org.hibernate.annotations.LazyCollection;
 import org.hibernate.annotations.LazyCollectionOption;
 
@@ -20,9 +23,12 @@ public class Orders implements Serializable {
 
   @Id @GeneratedValue private long id;
   private long userId;
-  private int totalPrice;
   private Date created = DateUtils.now();
   private Date updated;
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "orderId", fetch = FetchType.EAGER)
+  Set<ItemOrderInfo> itemOrderInfos = new HashSet<>();
+  @Transient
+  private int totalPrice;
 
   public long getId() {
     return id;
@@ -37,11 +43,11 @@ public class Orders implements Serializable {
   }
 
   public int getTotalPrice() {
-    return totalPrice;
-  }
-
-  public void setTotalPrice(int totalPrice) {
-    this.totalPrice = totalPrice;
+    int res = 0;
+    for (ItemOrderInfo itemOrderInfo : itemOrderInfos) {
+      res += itemOrderInfo.getPrice();
+    }
+    return res;
   }
 
   public Date getCreated() {
--- src/main/resources/sql/initialize_postgresql.sql
+++ src/main/resources/sql/initialize_postgresql.sql
@@ -7,37 +7,15 @@ insert into itemmeasure(name) values('м');
 insert into itemmeasure(name) values('м2');
 insert into itemmeasure(name) values('м3');
 
-CREATE OR REPLACE FUNCTION count_order_total_prices()
-RETURNS trigger AS '
+CREATE OR REPLACE FUNCTION do_order_deleted()
+  RETURNS trigger AS '
 DECLARE
-  var_orderid bigint;
 BEGIN
-  IF (TG_OP = ''DELETE'')
-  THEN
-    var_orderid := OLD.orderid;
-  ELSE
-    var_orderid := NEW.orderid;
-  END IF;
-  update public.orders
-  set totalprice = (SELECT
-                      SUM(price)
-                    FROM public.itemorderinfo
-                    where orderid = var_orderid),
-    updated = now()
-  where id = var_orderid;
-  IF (TG_OP = ''DELETE'')
-  THEN
-    return OLD;
-  ELSE
-    return NEW;
-  END IF;
+  delete from itemorderinfo where orderid = old.id;
+  return old;
 END;
 ' LANGUAGE 'plpgsql'
 VOLATILE;
-
-CREATE TRIGGER trg_update_order AFTER INSERT OR UPDATE OR DELETE
-ON public.itemorderinfo FOR EACH ROW
-EXECUTE PROCEDURE count_order_total_prices();
 
 CREATE OR REPLACE FUNCTION do_item_updated()
   RETURNS trigger AS '
@@ -49,6 +27,42 @@ END;
 ' LANGUAGE 'plpgsql'
 VOLATILE;
 
-CREATE TRIGGER item_trg_aft_updt AFTER UPDATE
+CREATE OR REPLACE FUNCTION do_order_updated()
+  RETURNS trigger AS '
+DECLARE
+  var_orderid bigint;
+BEGIN
+  IF (TG_OP = ''DELETE'')
+  THEN
+    var_orderid := OLD.orderid;
+  ELSE
+    var_orderid := NEW.orderid;
+  END IF;
+  update orders set updated = now() where id = var_orderid;
+  IF (TG_OP = ''DELETE'')
+  THEN
+    return OLD;
+  ELSE
+    return NEW;
+  END IF;
+END;
+' LANGUAGE 'plpgsql'
+VOLATILE;
+
+CREATE TRIGGER item_trg_bfr_updt BEFORE UPDATE
 ON item FOR EACH ROW
+WHEN (new.measure != old.measure
+      or new.name != old.name
+      or new.price != old.price)
 EXECUTE PROCEDURE do_item_updated();
+
+CREATE TRIGGER orders_trg_aft_del AFTER DELETE
+ON orders FOR EACH ROW
+EXECUTE PROCEDURE do_order_deleted();
+
+CREATE TRIGGER itemorderinfo_trg_aft_all AFTER INSERT OR UPDATE OR DELETE
+ON itemorderinfo FOR EACH ROW
+EXECUTE PROCEDURE do_order_updated();
+
+--drop TRIGGER item_trg_aft_updt ON item;
+--DROP FUNCTION do_item_updated();
